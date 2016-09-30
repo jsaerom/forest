b3dafc2086121be21fbb5301bc92c98e9d722d04
Joanna Anderson <joanna.saerom.anderson@gmail.com>
2016-09-30 11:12:51 -0700

    Passes all methods using database in Sighting class

--- src/main/java/Sighting.java
+++ src/main/java/Sighting.java
@@ -14,4 +14,78 @@ public class Sighting {
     this.location = _location;
     this.date = new Timestamp(new Date().getTime());
   }
+
+  public String getLocation() {
+    return this.location;
+  }
+
+  public Timestamp getDate() {
+    return this.date;
+  }
+
+  public int getId() {
+    return this.id;
+  }
+
+  public void setLocation(String _location) {
+    this.location = _location;
+  }
+
+  @Override
+  public boolean equals(Object otherSighting) {
+    if(!(otherSighting instanceof Sighting)) {
+      return false;
+    } else {
+      Sighting newSighting = (Sighting) otherSighting;
+      return this.getLocation().equals(newSighting.getLocation()) &&
+             this.getDate().getDay() == newSighting.getDate().getDay() &&
+             this.getId() == newSighting.getId();
+    }
+  }
+
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO sightings (location, date) VALUES (:location, now());";
+      this.id = (int) con.createQuery(sql, true)
+        .addParameter("location", this.location)
+        .executeUpdate()
+        .getKey();
+    }
+  }
+
+  public static List<Sighting> all() {
+    String sql = "SELECT * FROM sightings;";
+    try(Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql).executeAndFetch(Sighting.class);
+    }
+  }
+
+  public void update() {
+    try(Connection con = DB.sql2o.open()){
+    String sql = "UPDATE sightings SET (location) = (:location) WHERE id = :id;";
+    con.createQuery(sql)
+      .addParameter("location", this.location)
+      .addParameter("id", this.id)
+      .executeUpdate();
+    }
+  }
+
+  public static Sighting find(int id) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM sightings WHERE id = :id;";
+      Sighting animal = con.createQuery(sql)
+        .addParameter("id", id)
+        .executeAndFetchFirst(Sighting.class);
+      return animal;
+    }
+  }
+
+  public void delete() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "DELETE FROM sightings WHERE id = :id;";
+      con.createQuery(sql)
+        .addParameter("id", this.id)
+        .executeUpdate();
+    }
+  }
 }
--- src/test/java/SightingTest.java
+++ src/test/java/SightingTest.java
@@ -17,4 +17,59 @@ public class SightingTest {
     Sighting testSighting = new Sighting("Zone A");
     assertEquals(true, testSighting instanceof Sighting);
   }
+
+  @Test
+  public void getLocation_grabsLocationFromSighting_String() {
+    Sighting testSighting = new Sighting("Zone A");
+    assertEquals("Zone A", testSighting.getLocation());
+  }
+
+  @Test
+  public void equals_returnsTrueIfObjectIsSame_true() {
+    Sighting firstSighting = new Sighting("Zone A");
+    Sighting secondSighting = new Sighting("Zone A");
+    assertTrue(firstSighting.equals(secondSighting));
+  }
+
+  @Test
+  public void save_insertsObjectIntoDatabase_Sighting() {
+    Sighting testSighting = new Sighting("Zone A");
+    testSighting.save();
+    assertTrue(Sighting.all().get(0).equals(testSighting));
+  }
+
+  @Test
+  public void all_returnsAllInstancesOfSighting_true() {
+    Sighting firstSighting = new Sighting("Zone A");
+    firstSighting.save();
+    Sighting secondSighting = new Sighting("Zone A");
+    secondSighting.save();
+    assertEquals(true, Sighting.all().get(0).equals(firstSighting));
+    assertEquals(true, Sighting.all().get(1).equals(secondSighting));
+  }
+
+  @Test
+  public void getDate_returnsDate_true() {
+    Sighting testSighting = new Sighting("Zone A");
+    Timestamp newTime = new Timestamp(new Date().getTime());
+    assertEquals(newTime.getDay(), testSighting.getDate().getDay());
+  }
+
+  @Test
+  public void update_updatesLocation_true() {
+    Sighting testSighting = new Sighting("Zone A");
+    testSighting.save();
+    testSighting.setLocation("Zone B");
+    testSighting.update();
+    assertEquals("Zone B", testSighting.getLocation());
+  }
+
+  @Test
+  public void find_FindsSightingInstanceRelatedToId_true() {
+    Sighting firstSighting = new Sighting("Zone A");
+    firstSighting.save();
+    Sighting secondSighting = new Sighting("Zone B");
+    secondSighting.save();
+    assertEquals(secondSighting, Sighting.find(secondSighting.getId()));
+  }
 }
